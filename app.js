abc0592cbfffce019783b3911958580ff56f25ca
brent <brent.knop@hotmail.com>
2018-09-15 17:17:20 -0400

    Parse hrefs

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+//const jsdom = require("jsdom");
+//const { JSDOM } = jsdom;
 
+const cheerio = require('cheerio')
 
 var fs = require('fs'),
     path = require('path'),
-    filePath = path.join(__dirname, 'list.html');
+    filePath = path.join(__dirname, 'list-short.html');
 
-fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+fs.readFile(filePath, {encoding: 'utf-8'}, function(err, html){
     if (!err) {
-        console.log('received data: ' + data);
-        response.writeHead(200, {'Content-Type': 'text/html'});
+
+        const $ = cheerio.load(html);
+
+        $('li.has-thumb.span6 a.button').each(function() {
+          console.log($(this).attr('href'));
+        });
+
+        //$.html();
+
+
+
+        /*response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(data);
-        response.end();
+        response.end();*/
     } else {
         console.log(err);
     }
