1e3a80dac831d886c43a5e2d67c8360efa7ee354
brent <brent.knop@hotmail.com>
2018-09-15 23:33:18 -0400

    Parser pretty much working

--- parser.js
+++ parser.js
@@ -1,3 +1,5 @@
+const cheerio = require('cheerio');
+
 var fs = require('fs');
 var path = require('path');
 
@@ -23,4 +25,32 @@ function getFilesFromDir(dir, fileTypes) {
 }
 
 //print the txt files in the current directory
-getFilesFromDir("./attendees", [".html"]).map(console.log);
+//getFilesFromDir("attendees/", [".html"]).map(console.log);
+getFilesFromDir("attendees/", [".html"]).map(function(file) {
+
+  fs.readFile('attendees/' + file, {encoding: 'utf-8'}, function(err, html) {
+      if (!err) {
+
+        const $ = cheerio.load(html);
+
+        var csv = '',
+          name = $('p.metadataTitle span').first().text().trim(),
+          companyTitle = [];
+
+        companyTitle = $('span.ellipsis.subtitle').html().split('<br>');
+
+        csv += '"' + name + '",';
+        csv += '"' + companyTitle[0] + '",';
+        csv += '"' + companyTitle[1] + '"';
+        csv +=  "\n";
+
+        console.log(csv);
+
+        /*fs.appendFile('attendees.csv', csv, function() {
+          //console.log(file, csv);
+        });*/
+      } else {
+          console.log(err);
+      }
+  });
+});
